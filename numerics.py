46833b426029447933abed56a7d7305eab77c190
Torrey Betts <tbetts@infragistics.com>
2017-10-08 15:15:29 -0400

    - IRC numeric output now supports 372, 375 and 376 - Reformatted console messages - Minor refactoring

--- numerics.py
+++ numerics.py
@@ -1,5 +1,13 @@
 import hues
 
+#Message of MOTD
+def numeric_372(irc, config, host, user, message):
+    hues.log(hues.huestr("[MOTD] " + message).black.colorized)
+
+#Start of MOTD
+def numeric_375(irc, config, host, user, message):
+    hues.log(hues.huestr("[MOTD] " + message).black.bold.colorized)
+
+#End of MOTD
 def numeric_376(irc, config, host, user, message):
-    hues.log(hues.huestr("Sent: JOIN").magenta.bold.colorized)
-    irc.send(config["irccommands"]["join"].format(config["channel"]))
+    hues.log(hues.huestr("[MOTD] " + message).black.bold.colorized)
